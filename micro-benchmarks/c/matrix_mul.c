f08c98ec1d5f0deca8496740777a0d59a05ee016
Michael Zhang <heronalps@gmail.com>
2020-11-12 06:08:43 -0800

    mz - Add matrix_mul.c

--- micro-benchmarks/c/matrix_mul.c
+++ micro-benchmarks/c/matrix_mul.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
-#define N 128
+#include <stdlib.h>
+#include <time.h>
+#define N 256
 
-void multiply(int mat1[N][N], int mat2[N][N], int res[N][N]) {
+int nsleep(long milliseconds) {
+    struct timespec req, rem;
+
+    if (milliseconds > 999) {
+        req.tv_sec = (int) (milliseconds / 1000);
+        req.tv_nsec = (milliseconds - ((long) req.tv_sec * 1000)) * 1000000;
+    }
+    else {
+        req.tv_sec = 0;
+        req.tv_nsec = milliseconds * 1000000;
+    }
+
+    return nanosleep(&req, &rem);
+}
+
+void multiply(double mat1[][N], double mat2[][N], double res[][N]) {
     int i, j, k;
+    int ret = nsleep(2500);
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
             res[i][j] = 0;
@@ -13,9 +31,36 @@ void multiply(int mat1[N][N], int mat2[N][N], int res[N][N]) {
     }
 }
 
+double genRandom() {
+    return (double) rand() / RAND_MAX * 2.0 - 1.0;
+}
+
 int main() {
     // Initialize matrix by doubles
-    int res[N][N];
-    multiply(mat1, mat2, res);
+    srand(time(NULL));
+
+    double res[N][N];
+    double mat1[N][N];
+    double mat2[N][N];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            mat1[i][j] = genRandom();
+            mat2[i][j] = genRandom();
+        }
+    }
+    
+    clock_t c0 = clock();
+    for (int i = 0; i < 12; i++) {
+        multiply(mat1, mat2, res);
+    }
+    clock_t c1 = clock();
+    double runtime_diff_ms = (c1 - c0) * 1000. / CLOCKS_PER_SEC;
+    
+    printf ("Time : %f \n", runtime_diff_ms);
+    // for (int i = 0; i < N; i++) {
+    //     for (int j = 0; j < N; j++) {
+    //         printf("res : %f\n", res[i][j]);
+    //     }
+    // }
     return 0;
 }
