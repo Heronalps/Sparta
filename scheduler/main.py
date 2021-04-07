c34bf4ec2e9869f07596092fc0d6f1e9850d04cd
heronalps <heronalps@gmail.com>
2021-04-07 12:26:40 -0700

    mz - Modify main.py

--- scheduler/main.py
+++ scheduler/main.py
@@ -3,15 +3,16 @@ from scheduler import Scheduler
 
 def main():
     s = Scheduler("../micro-benchmarks/python/Image_cls.py", "temp_log_path", 75.0)
-    for _ in range(10):
+    for t in range(10):
+        print ("{} time(s) of execution".format(t + 1))
         s.run()
         print (repr(s))
         count = 0
         for i in s.temp_log:
             if i > s.temp_threshold:
                 count += 1
+        print ("len : {}".format(len(s.temp_log)))
         print ("Accuracy : {}".format(1 - (count / len(s.temp_log))))
-    # print (repr(s))
 
 if __name__ == "__main__":
     main()
